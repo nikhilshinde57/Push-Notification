fb5a77cd66f76402672177abdd7b6c4f0297070a
Nikhil Shinde <nikhilshinde57@gmail.com>
2018-12-23 11:36:25 +0530

    Updated Server.js - commented code clean up

--- App Server/server.js
+++ App Server/server.js
@@ -10,33 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-f
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.urlencoded());   // parse application/x-www-form-urlencoded
 
-
-// Connect to server
-// var io = require('socket.io-client')
-// var socket = io.connect('http://localhost:4000', {reconnect: true});
-
-
-
-// // Add a connect listener
-// socket.on('connect', function(test) {    
-
-//     console.log("Connected to http://localhost:4000")
-
-//     socket.emit("online",{
-//         "username":"nikhilshinde57",
-//         "userid":12
-//     });
-
-//     socket.on("message",(req)=>{
-//         console.log(req.toString());
-//         socket.emit("offline",{
-//             "username":"nikhilshinde57",
-//             "userid":12
-//         })
-//     })
-// });
-
-
 app.use('/api',api);
 
 app.listen(PORT,()=>{
